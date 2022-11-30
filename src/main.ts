8b9a9bc158f8f70ffa099387650b2232d5eb1519
guojingzhou <guojingzhou@huayun.com>
2022-11-30 22:59:06 +0800

    feat: 使用pinia

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import pinia from './store'
 
 const app = createApp(App)
-app.use(router).mount('#app')
+app.use(router).use(pinia).mount('#app')
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    Home
+    Home {{ count }} - {{ double }} <button @click="increment">+</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useCounterStore } from '@/store/modules/counter';
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore();
+const { count, double } = storeToRefs(store)
+const { increment } = store
 
 </script>
 
