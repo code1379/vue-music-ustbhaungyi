12b0f18559f700b596e77f6e29397883f59076ba
guojingzhou <guojingzhou@huayun.com>
2022-11-30 22:37:02 +0800

    feat: 使用vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path' // npm i @types/node
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": "./src"
+      "@":path.resolve(__dirname, "src")
     }
   }
 })
